851f4540ecdaa4e41e083806a8c96f4a4ecfc74b
Julian Baumgartner <jbaumgartner93@gmail.com>
2023-09-30 09:10:01 -0400

    Update

--- BeckerChecklistApp/checklist/apps.py
+++ BeckerChecklistApp/checklist/apps.py
@@ -1,7 +1,7 @@
-from django.conf import settings
 from django.apps import AppConfig
 
-from checklist.gcp import gcp_storage_sync()
+from checklist.gcp import gcp_storage_sync
+
 
 class ChecklistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
